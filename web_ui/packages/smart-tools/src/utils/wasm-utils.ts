b1d80224ff4ae05ab1d59ea290cbaa0df44506da
Mark Redeman <mark.s.redeman@intel.com>
2025-09-02 11:53:54 +0200

    Try webgpu

--- web_ui/packages/smart-tools/src/utils/wasm-utils.ts
+++ web_ui/packages/smart-tools/src/utils/wasm-utils.ts
@@ -7,24 +7,24 @@ export interface SessionParameters {
     wasmRoot?: string | Record<string, string>;
 }
 
-const wasmPaths = {
-    'ort-wasm.wasm': new URL('../../../../node_modules/onnxruntime-web/dist/ort-wasm.wasm', import.meta.url).toString(),
-    'ort-wasm-simd.wasm': new URL(
-        '../../../../node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
-        import.meta.url
-    ).toString(),
-    'ort-wasm-threaded.wasm': new URL(
-        '../../../../node_modules/onnxruntime-web/dist/ort-wasm-threaded.wasm',
-        import.meta.url
-    ).toString(),
-    'ort-wasm-simd-threaded.wasm': new URL(
-        '../../../../node_modules/onnxruntime-web/dist/ort-wasm-simd-threaded.wasm',
-        import.meta.url
-    ).toString(),
-};
+// const wasmPaths = {
+//     'ort-wasm.wasm': new URL('../../../../node_modules/onnxruntime-web/dist/ort-wasm.wasm', import.meta.url).toString(),
+//     'ort-wasm-simd.wasm': new URL(
+//         '../../../../node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
+//         import.meta.url
+//     ).toString(),
+//     'ort-wasm-threaded.wasm': new URL(
+//         '../../../../node_modules/onnxruntime-web/dist/ort-wasm-threaded.wasm',
+//         import.meta.url
+//     ).toString(),
+//     'ort-wasm-simd-threaded.wasm': new URL(
+//         '../../../../node_modules/onnxruntime-web/dist/ort-wasm-simd-threaded.wasm',
+//         import.meta.url
+//     ).toString(),
+// };
 
 export const sessionParams: SessionParameters = {
     numThreads: 0,
     executionProviders: ['cpu'],
-    wasmRoot: wasmPaths,
+    //wasmRoot: wasmPaths,
 };
